ec66ee774c4ef6cc227c217963d397c7bbc5a57d
xeffyofficial <xeffy@xeffy.io>
2026-07-22 16:06:51 +0900

    Delete xef-supply-api/api/total-supply.js

--- xef-supply-api/api/total-supply.js
+++
@@ -1,8 +0,0 @@
-const { TOTAL_SUPPLY } = require("../lib/vesting");
-
-// GET /api/total-supply -> 순수 숫자 텍스트 (예: 6000000000)
-// 코인게코/CMC의 "Total Supply API" 필드에 그대로 넣으면 되는 표준 형식입니다.
-module.exports = (req, res) => {
-  res.setHeader("Content-Type", "text/plain");
-  res.status(200).send(String(TOTAL_SUPPLY));
-};
